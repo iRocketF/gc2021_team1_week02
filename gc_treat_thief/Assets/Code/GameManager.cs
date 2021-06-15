1ad2d8db6dc080f6f57991eba2ff596af6685632
Eero Salmi <eswsalmi@gmail.com>
2021-06-15 13:03:12 +0300

    Music fade, player model implemented

--- gc_treat_thief/Assets/Code/GameManager.cs
+++ gc_treat_thief/Assets/Code/GameManager.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private AudioSource mainMusic;
+    [SerializeField]
+    private AudioSource chaseMusic;
+
+    public bool pIsChased;
+    public bool isChaseOn;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,25 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateMusic();
     }
+
+    void UpdateMusic()
+    {
+        if (pIsChased && !isChaseOn)
+        {
+            StartCoroutine(FadeMusic.StartFade(mainMusic, 2f, 0f));
+            StartCoroutine(FadeMusic.StartFade(chaseMusic, 2f, 0.5f));
+            isChaseOn = true;
+        }
+        else if (!pIsChased && isChaseOn)
+        {
+            StartCoroutine(FadeMusic.StartFade(chaseMusic, 2f, 0f));
+            StartCoroutine(FadeMusic.StartFade(mainMusic, 2f, 0.5f));
+            isChaseOn = false;
+        }
+    }
+
+
+
 }
