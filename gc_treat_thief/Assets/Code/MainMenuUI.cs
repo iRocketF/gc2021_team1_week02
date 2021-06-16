2060f4d062f3e5844716cdd93ac36637ca1ea5b8
Laura Huovinen <laura.huovinen@outlook.com>
2021-06-16 11:51:35 +0300

    Main menu started
    - Main menu has basic functionality, but can't load the game yet since we don't have a game scene
    - Need to add settings?

---
+++ gc_treat_thief/Assets/Code/MainMenuUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    private const int gameScene = 1;
+
+    public void StartGame()
+    {
+        Debug.Log("Play pressed, start the game");
+        //SceneManager.LoadScene(gameScene);
+    }
+
+    public void Settings()
+    {
+        Debug.Log("Settings pressed, open the settings");
+        // Add functionality later
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit pressed, quit the game");
+        Application.Quit();
+    }
+}
